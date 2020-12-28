3288dbf05507c016063a00e5aee152b9c284618d
iam-david <vanmelkebekedavid@gmail.com>
2020-12-29 00:45:10 +0100

    First little test

--- mixedmessage.js
+++ mixedmessage.js
@@ -1,1 +1,8 @@
-console.log('Hello World!');
+let artist = ['Eminem', 'Michael Jackson', 'Phill Collins', 'Dolly Parton', 'Blondie'];
+let verb = ['created', 'almost deleted', 'wrote', 'hates', 'loves', 'banned'];
+let song = ['Stan', 'Thriller', 'On my way', 'Diamonds', '99 Luftballon'];
+let adverb = ['on the toilet', 'from his own memory', 'while he was riding a waterscooter', 'almost as much as I do', 'in a capella', 'from Spotify'];
+
+let all_arrays = [artist, verb, song, adverb];
+
+console.log(all_arrays[0][0]);
