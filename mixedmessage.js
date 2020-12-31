ffd99cff713140b569010f8daaa70f9279bcabf6
iam-david <vanmelkebekedavid@gmail.com>
2020-12-31 23:26:31 +0100

    Created default parameters and a function for randomizing

--- mixedmessage.js
+++ mixedmessage.js
@@ -4,8 +4,6 @@ const verb = ['created', 'almost deleted', 'wrote', 'hates', 'loves', 'banned', 
 const song = ['She Loves You', 'Satisfaction', 'Your Song', 'Fantasy', 'Into the Groove', 'People', 'Billie Jean', 'Shake it off', 'As', 'If You Leave Me Now', 'I Will Always Love You', 'Hi, Hi, Hi', 'Heartbreak Hotel', 'That\'s The Way Love Goes', 'Hot Legs'];
 const adverb = ['on the toilet', 'from his own memory', 'while he was riding a waterscooter', 'almost as much as I do', 'in a capella', 'from Spotify', 'on a toy xylophone', 'after a visit at the dentist', 'on a Nokia 3210', 'to sound like a cat', 'after watching a documentary about balloons', 'from the family chest of Queen mom', 'in your ears', 'at a high pitch', 'on a groovy beat'];
 
-// all arrays in one array to keep an overview
-const allArrays = [artist, verb, song, adverb];
 
 // make it possible to add a value to an array
 const addItem = (array, value) => array.push(value);
@@ -13,26 +11,14 @@ const addItem = (array, value) => array.push(value);
 // delete a value from an array
 const removeItem = (array, value) => array = array.filter(item => item !== value);
 
-// return a random sentence
-const createSentence = (customArtist, customVerb, customSong, customAdverb) => {
-    let randomArtist = allArrays[0][Math.floor(Math.random() * artist.length)];
-    let randomVerb = allArrays[1][Math.floor(Math.random() * verb.length)];
-    let randomSong = allArrays[2][Math.floor(Math.random() * song.length)];
-    let randomAdverb = allArrays[3][Math.floor(Math.random() * adverb.length)];
-    // a user can add his custom values
-    if (customArtist) {
-        randomArtist = customArtist;
-    }
-    if (customVerb) {
-        randomVerb = customVerb;
-    }
-    if (customSong) {
-        randomSong = customSong;
-    }
-    if (customAdverb) {
-        randomAdverb = customAdverb;
-    }
-    let sentence = randomArtist + ' ' + randomVerb + ' ' + randomSong + ' ' + randomAdverb;
+// takes a random item of an array
+const getRandomValue = array => {
+    return array[Math.floor(Math.random() * array.length)];
+}
+
+// return a random sentence with random values or custom values
+const createSentence = (customArtist = getRandomValue(artist), customVerb = getRandomValue(verb), customSong = getRandomValue(song), customAdverb = getRandomValue(adverb)) => {
+    let sentence = customArtist + ' ' + customVerb + ' ' + customSong + ' ' + customAdverb;
     return sentence;
 }
 
